3af6d466a25d5d63a069ae15f7459164d112d998
Deise Domingues <dominguesdeise@gmail.com>
2024-10-06 08:03:07 -0300

    feat(p1-pdmn): cria componente principal e faz hello

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'; //em node_modules
+
+const App = () => {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col text-start">
+                    <h1>Hello, tarefas</h1>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default App;
